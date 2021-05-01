0ed6352cb4d031bfd0a0b23c57423f487c71d548
Mohit Jain <jainmohit0807@gmail.com>
2021-05-01 14:06:32 +0530

    chnages

---
+++ src/people.js
@@ -0,0 +1,15 @@
+export let people = [{
+    name: "John",
+    age: 45,
+    haircolor: "blonde",
+},{
+    name: "Mohit",
+    age: 25,
+    haircolor: "black",
+},
+{
+    name: "Harshita",
+    age: 25,
+    haircolor: "black",
+}
+]
---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import {people} from './people';
+
+let app = express();
+
+app.get('/hello',(req,res) => {
+    res.send("Hello! ");
+} )
+
+app.get('/people',(req,res)=> {
+    res.json(people);
+})
+
+app.get('/people/:name',(req,res)=> {
+    let {name} = req.params;
+    let person = people.find(x => x.name === name);
+    res.json(person);
+})
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000");
+})
